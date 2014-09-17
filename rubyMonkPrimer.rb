47a3dc9fa8a88eb468f860771a0196cf81a90c5c
Taiwo Ayanleye <onlyyouandty@yahoo.com>
2014-09-17 11:26:53 +0100

    initial commit

---
+++ rubyMonkPrimer.rb
@@ -0,0 +1,15 @@
+def find_frequency(sentence, word) 
+end
+
+def length_finder(input_array)
+  # puts input_array.map { |x|  x.length}
+  puts input_array.map {|element| element.length}
+end
+
+length_finder(['I','am','genius'])
+
+
+# def sum_of_cubes(a, b)
+# 	puts (a..b).inject(0) { |sum, x| sum += (x*x*x) }
+# end
+# sum_of_cubes 1, 3
