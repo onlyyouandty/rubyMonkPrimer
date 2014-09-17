b2ff7d154bf05eb4380dc48611ce6393c809fb22
Taiwo Ayanleye <onlyyouandty@yahoo.com>
2014-09-17 12:18:52 +0100

    updated solution till split_length

--- rubyMonkPrimer.rb
+++ rubyMonkPrimer.rb
@@ -1,12 +1,27 @@
-def find_frequency(sentence, word) 
-end
+def random_select(array, n)
 
-def length_finder(input_array)
-  # puts input_array.map { |x|  x.length}
-  puts input_array.map {|element| element.length}
 end
+random_select( [1, 2, 3, 4, 5], 2 )
 
-length_finder(['I','am','genius'])
+# def sort_string(string)
+# 	puts string.split.sort_by {|x| x.length}.join(' ')
+# end
+# sort_string("Sort words in a sentence")
+
+# def find_frequency(sentence, word) 
+# 	puts sentence.downcase.split.count(word)
+# 	# arr = []
+# 	# arr = sentence.downcase.split(' ')
+# 	# puts arr.count(word)
+# end
+# find_frequency('Ruby is The best language in the World','the')
+
+
+# def length_finder(input_array)
+#   # puts input_array.map { |x|  x.length}
+#   puts input_array.map {|element| element.length}
+# end
+# length_finder(['I','am','genius'])
 
 
 # def sum_of_cubes(a, b)
